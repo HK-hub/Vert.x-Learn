fc08041dd9429ac490bdce37aca3cd1967ac7430
HK-hub <3161880795@qq.com>
2023-10-09 21:08:08 +0800

    feat: JDK工具JPS,Jstack,jinfo学习

--- ChatRoom/src/main/java/com/hk/im/server/EventBusServer.java
+++ ChatRoom/src/main/java/com/hk/im/server/EventBusServer.java
@@ -4,14 +4,14 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerRequest;
-import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.bridge.PermittedOptions;
+import io.vertx.ext.web.RequestBody;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
 import io.vertx.ext.web.handler.sockjs.SockJSBridgeOptions;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
-import io.vertx.ext.web.handler.sockjs.SockJSHandlerOptions;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author : HK意境
@@ -23,11 +23,19 @@ import io.vertx.ext.web.handler.sockjs.SockJSHandlerOptions;
  * @Modified :
  * @Version : 1.0
  */
+@Slf4j
 public class EventBusServer extends AbstractVerticle {
 
-    public static void main(String[] args) {
+    public static Object LOCK1 = new Object();
+
+    public static Object LOCK2 = new Object();
+
+
+    public static void main(String[] args) throws InterruptedException {
 
         Vertx.vertx().deployVerticle(new EventBusServer());
+        // 启动死锁
+        deadlockTest();
     }
 
     @Override
@@ -46,6 +54,11 @@ public class EventBusServer extends AbstractVerticle {
             .addOutboundPermitted(permittedOptions);
 
         router.route("/chat/*")
+            .handler(ctx -> {
+                RequestBody body = ctx.body();
+                JsonObject entries = body.asJsonObject();
+                log.info("chat room received message:{}", entries);
+            })
             .subRouter(sockJSHandler.bridge(bridgeOptions));
         // 返回静态页面
         router.route().handler(StaticHandler.create("static"));
@@ -54,4 +67,42 @@ public class EventBusServer extends AbstractVerticle {
         // 启动服务器，监听端口
         server.requestHandler(router).listen(8080);
     }
+
+
+
+    public static void deadlockTest() throws InterruptedException {
+
+        Thread threadA = new Thread(() -> {
+
+            synchronized (LOCK1) {
+                System.out.println("已经获取锁1");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+
+                }
+                synchronized (LOCK2) {
+                    System.out.println("已经获取锁2");
+                }
+            }
+
+        }, "deadlock-test-thread-0");
+
+
+        Thread threadB = new Thread(() -> {
+            synchronized (LOCK2) {
+                System.out.println("已经获取锁1");
+                synchronized (LOCK1) {
+                    System.out.println("已经获取锁2");
+                }
+            }
+        }, "deadlock-test-thread-1");
+
+        // 启动线程
+        threadA.start();
+        Thread.sleep(1000);
+        threadB.start();
+
+    }
+
 }
